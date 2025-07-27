0f891affd33b1adc88bd070370131e29441e66bb
Felix Y. Quispe Huamani <felixyoelquispeh@gmail.com>
2025-07-27 15:01:09 -0500

    Delete sisventas directory

--- sisventas/vista/proveedores/eliminar.php
+++
@@ -1,7 +0,0 @@
-<?php
-ob_start();
-require_once $_SERVER['DOCUMENT_ROOT'] . "/sisventas/controlador/ProveedorControlador.php";
-$controlador = new ProveedorControlador();
-$controlador->eliminarProveedor($_GET['id']);
-header("Location: listado.php");
-exit;
